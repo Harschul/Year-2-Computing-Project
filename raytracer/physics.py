a5a644c19b4b26093cc2e2e0cc0e005b680cffec
harshul <hc1424@ic.ac.uk>
2026-05-26 18:18:03 +0100

    Added working refraction module

---
+++ raytracer/physics.py
@@ -0,0 +1,42 @@
+"""Physics module that implements refractions at incidence points"""
+
+import numpy as np
+
+def angle(a, b):
+    """Calculates the angle between two position vectors"""
+    cos_theta = np.dot(a, b) / (np.linalg.norm(a) * np.linalg.norm(b))
+    cos_theta = np.clip(cos_theta, -1.0, 1.0)
+    return np.arccos(cos_theta)
+
+
+def refract(direc, normal, n_1, n_2):
+    """Calculate the new refracted ray direction"""
+
+    direc = np.array(direc)
+    normal = np.array(normal)
+
+    direc_hat = direc / np.linalg.norm(direc)
+    normal_hat = normal / np.linalg.norm(normal)
+
+    n_ratio = n_1 / n_2
+    theta_i = angle(direc, -normal_hat)
+    sin_theta_r = n_ratio * np.sin(theta_i)
+
+    if abs(sin_theta_r) > 1:
+        return None
+    theta_r = np.arcsin(sin_theta_r)
+    if np.isclose(theta_i, 0.0):
+        return -normal_hat
+
+    u = np.sin(theta_r)
+    v = -np.cos(theta_r)
+    direc_new_basis = np.array([u, v])
+
+    tangent = direc_hat - np.dot(direc_hat, normal_hat) * normal_hat
+    tangent_norm = np.linalg.norm(tangent)
+    tangent_hat = tangent / tangent_norm
+
+    basis_2d = np.column_stack((tangent_hat, normal_hat))
+    new_direc_r = basis_2d @ direc_new_basis
+
+    return new_direc_r
