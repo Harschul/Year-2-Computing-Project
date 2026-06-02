815044e2e0bca1fb52a8615e11ecbf48975d9bc1
harshul <hc1424@ic.ac.uk>
2026-06-02 19:20:31 +0100

    Fixed all internals

--- raytracer/rays.py
+++ raytracer/rays.py
@@ -1,7 +1,6 @@
 """Rays and their properties"""
-from copy import deepcopy
-import numpy as np
 
+import numpy as np
 
 class Ray:
     """Ray object"""
@@ -13,13 +12,39 @@ class Ray:
         """
 
         if pos is None:
-            pos = [0, 0, 0]
+            pos = np.array([0, 0, 0], dtype = float)
 
         if direc is None:
-            direc = [0, 0, 1]
+            direc = np.array([0, 0, 1], dtype = float)
 
-        self.__pos = [np.array(pos)]
-        self.__direc = [np.array(direc)]
+        pos = np.array(pos, dtype = float)
+        direc = np.array(direc, dtype = float)
+        self.dimension_check(pos, direc)
+        self.normalise(direc)
+        self.__pos = [pos]
+        self.__direc = direc
+
+
+    def normalise(self, vector):
+        """Normalizes a vector and alters it"""
+        norm = np.linalg.norm(vector)
+        if norm == 0:
+            raise ValueError("The magnitude of the direction vector is 0")
+        vector[:] = vector/norm
+        return vector
+
+
+    def dimension_check(self, position, direction):
+        """Checks dimensions of input arrays"""
+
+        if len(direction) < 3:
+            raise TypeError("The direction inserted is less than 3D")
+        if len(direction) > 3:
+            raise TypeError("The direction inserted is greater than 3D")
+        if len(position) < 3:
+            raise TypeError("The position inserted is less than 3D")
+        if len(position) > 3:
+            raise TypeError("The position inserted is greater than 3D")
 
     def pos(self):
         """Return the latest position of the ray"""
@@ -27,13 +52,15 @@ class Ray:
 
     def direc(self):
         """Return the latest direction of the ray"""
-        return self.__direc[-1].copy()
+        return self.__direc
 
     def append(self, pos, direc):
         """Add a new position and direction to the ray"""
+        self.dimension_check(pos, direc)
+        self.normalise(direc)
         self.__pos.append(np.array(pos))
-        self.__direc.append(np.array(direc))
+        self.__direc = np.array(direc)
 
     def vertices(self):
         """Return the position history of the ray """
-        return deepcopy(self.__pos)
+        return self.__pos
